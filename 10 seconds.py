9a0a6784f3a3180299c711bdf8c96e0de5d2a785
ValiantsEdge <petergallogly0@gmail.com>
2024-05-22 18:57:59 -0400

    Initial Commit Functional start and acknowledge buttons for starting random intervals and 10 second breaks Functional stop button for concluding a session and being told how long it was and number of breaks

---
+++ 10 seconds.py
@@ -0,0 +1,75 @@
+import random
+from tkinter import *
+import time
+import datetime
+
+breakCounter = 0
+startTime = datetime.datetime.now()
+
+def randomTime():
+    # randomTimeInt = random.randint(90, 450)
+    randomTimeInt = random.randint(1, 2)
+    # Wait for length = randomTimeInt
+    time.sleep(randomTimeInt)
+    print("Slept for " + str(randomTimeInt) + " seconds")
+    # Make label show break prompt text
+    text.config(text="Time for a 10 second break")
+    text.update()
+
+def changeText(whichButton, stopTime = None):
+    global breakCounter
+    if whichButton == "Ack":
+        text.config(text="Started 10 second break")
+        text.update()
+        breakCounter += 1
+        time.sleep(5)
+        text.config(text="10 second break over")
+        text.update()
+    if whichButton == "Stop":
+        breakText = "You took " + str(breakCounter) + " breaks"
+        timeDifference = stopTime - startTime
+        timeDifferenceMins = divmod(timeDifference.seconds, 60)
+        if timeDifferenceMins[0] < 60:
+            totalTimeMinsSec = "Your session lasted "+ str(timeDifferenceMins[0])+ " minutes and "+ str(timeDifferenceMins[1])+ " seconds \n" + breakText
+            text.config(text=totalTimeMinsSec)
+        else:
+            timeDifferenceHours = divmod(timeDifferenceMins, 60)
+            totalTimeHourMin = "Your session lasted "+ str(timeDifferenceHours[0])+ " hours and ", str(timeDifferenceHours[1])+ " minutes \n" + breakText
+            text.config(text=totalTimeHourMin)
+    
+# Root window
+root = Tk()
+root.title("Randomly Spaced 10 Seconds")
+root.minsize(300,300)
+root.maxsize(600,600)
+root.geometry("300x300+50+50")
+
+# Label
+text = Label(root, text="")
+text.pack()
+
+# Start button
+def startFunc():
+    startTime = datetime.datetime.now()
+    randomTime()
+    
+startButton = Button(root, text="Start", command=startFunc)
+startButton.pack()
+
+# Stop button
+def stopFunc():
+    stopTime = datetime.datetime.now()
+    changeText("Stop",stopTime)
+
+stopButton = Button(root, text="Stop", command=stopFunc)
+stopButton.pack()
+
+# Acknowledge button
+def ackFunc():
+    changeText("Ack")
+    randomTime()
+
+ackButton = Button(root, text="Acknowledge", command=ackFunc)
+ackButton.pack()
+
+root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Random-10-Second-Break
-Repository for Tkinter based random 10 second break app
+Repository for Tkinter based random 10 second break app inspired by Andrew Huberman's "Neuroplasticity Super Protocol" 
+
+Dr.Huberman claims that implementing random micro-rest periods can help one learn faster by leveraging neural repititions.
+
+Here is an excerpt from his newsletter (https://www.hubermanlab.com/newsletter/teach-and-learn-better-with-a-neuroplasticity-super-protocol):
+"Studies (in humans) have shown that when we are trying to learn something, if we pause every so often for 10seconds and do nothing during the pause, neurons in the hippocampus and cortex—areas of the brain involved in learning and memory, engage the same patterns of neural activity that occurred during the actual activity of reading, musical practice, skill training, etc. but 10X faster—meaning you get 10X neural repetitions completed during the pause. These “gap-effects” are similar to what happens in a deep sleep. The takeaway: randomly introduce 10 second pauses during learning. “How often?” I get asked. A ratio of approximately 1 pause per every 2 minutes of learning is good but remember, distributed at random, so not every 2 minutes on the minute."
+
+This app choses a random time interval between 1 and a half minutes (90 seconds) and 4 and a half minutes (270 seconds) and tells you, through a GUI display when to take a 10 second break, and when to resume working.
